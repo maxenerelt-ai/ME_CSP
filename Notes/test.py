366fb64f4c9714b80076a9e148ba7581889b4f30
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-02 13:11:41 -0600

    first commit

---
+++ Notes/test.py
@@ -0,0 +1,18 @@
+# ME 6th Github Connection
+
+print("hi")
+
+pi = 3.14
+print(pi)
+pi = "Monkey"
+print(pi)
+monaonanw = "gkjgsgksfdkghdsgfj"
+
+print(monaonanw)
+
+color = input("What color is the sky?\n")
+
+animal = input("What is my pet parrots name: ")
+
+print("What is the average wing velocity of an unlaiden shadow")
+velocity = input()
