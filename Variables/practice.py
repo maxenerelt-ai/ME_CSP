4e652b626ab13ae5b0b35d853c9b0241c64b311a
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-03 13:45:11 -0600

    Varuavel practive

---
+++ Variables/practice.py
@@ -0,0 +1,23 @@
+# ME 6th Variables Practice
+
+name = input("What is your name?\n")
+
+numeral = input("What is a number between 1 and 10?\n")
+
+number = input("What is a number between 100 and 1000?\n")
+
+breakfast = input("What did you have for breakfast?\n")
+
+color = input("What is your favorite color?\n")
+
+school = input("What is the schools name?\n")
+
+year = input("What year is it?\n")
+
+eye = input("What color are your eyes?\n")
+
+age = input("How old are you?\n")
+
+subject = input("Whats your favorite color?\n")
+
+print("Your name is", name, "The number you picked was", numeral, "Another number you picked is", number, "")
