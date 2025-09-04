df67481d006820ea158793f9a9513e6066b9cd2e
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-04 13:43:03 -0600

    Expressions notes

---
+++ Notes/expressions.py
@@ -0,0 +1,33 @@
+# ME 6th Expressions Notes
+
+# What is an algorithm?
+    #A set of instructions to accomplish a specific task
+name = input("What is your name?")
+print("Hello", name)
+
+#Algorithm for Area
+length = 5
+width= 20
+area = length * width
+print(length, "X", width, "=", area)
+
+#List steps in an algorithm
+    #1.determine variables needed 2.Complete processes needed 3.provide an output
+
+#List all of the different mathematical operators(Give me the symbol and tell me what it does)
+    # + = add, - = minus, *= Times, / = division
+
+num1 = 234
+num2 = 293
+num3 = 42
+num4 = 325
+average = num1+num2+num3+num4
+answer = average/4
+
+print(num1, "+", num2 , "+", num3 , "+", num4, "=", answer)
+
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+    #
+
+#Why are expressions so important in programming
+    #
