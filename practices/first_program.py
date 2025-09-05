6d57b9c43a3f7367bd7a1e014f09c46c1e37bfab
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-05 16:19:33 -0600

    First Program

---
+++ practices/first_program.py
@@ -0,0 +1,7 @@
+# ME 6th First Program
+
+name = input("Whats your name\n")
+
+color = input("Whats your favortite color\n")
+
+print("Hello there", name, "nice to meet you it looks like your favorite color is", color, "thats a cool color")
