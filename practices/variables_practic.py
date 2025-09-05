4a585adfa35af45a33dce12225fe658beeb6abe1
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-05 16:09:53 -0600

    Variables Practice

--- practices/variables_practic.py
+++ practices/variables_practic.py
@@ -1,2 +1,23 @@
 # ME 6th Variables Practice
 
+name = input("Whats your name\n")
+
+number = input("A number between 1 and 10\n")
+
+number_2 = input("A number between 100 and 1000\n")
+
+breakfast = input("What you had for breakfast\n")
+
+color = input("Whats your favorite color\n")
+
+school = input("What the schools name\n")
+
+year = input("What year is it\n")
+
+eye = input("Your eye color\n")
+
+age = input("Whats your age\n")
+
+subject = input("Your favorite subject in school\n")
+
+print("Hi there", name, "you picked the number", number, "another number you picked is", number_2, "for breakfast you had", breakfast, "your favorite color is", color, "you go to", school, "the year it is", year, "your eye color is", eye, "your age is", age, "and you favorite subject in school is", subject)
