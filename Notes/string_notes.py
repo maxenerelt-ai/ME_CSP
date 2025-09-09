7e3bdf0294efadaf3e319a282cf2d106ca811b39
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-09 13:42:53 -0600

    Finished String Notes

--- Notes/string_notes.py
+++ Notes/string_notes.py
@@ -2,7 +2,8 @@
 
 #I Did It
 
-# Class Notes: String- Collection of symblos held together by quotation marks, string is only data type that can hold letters, types of quotation doesnt matter as long as they're the same
+# Class Notes: String- Collection of symblos held together by quotation marks, string is only data type that can hold letters, types of quotation doesnt matter as long as they're the same,
+# Bug- any error in your code that keeps it from runnning or makes it run improperly,
 
 #String Exapmles
 
@@ -23,6 +24,7 @@ print("Welcome to my program", full_name + "!")
     #.upper() turns whole thing too upercase letters .lower() makes all lowercase
     #.capitalize() makes first letter capital .title() makes every first letter per space capital
     # To get rid of space use +
+    # Bug- any error in your code that keeps it from runnning or makes it run improperly
 
 #What makes something a string?
     # Symbols inside quotation marks
@@ -33,9 +35,13 @@ print("Welcome to my program", full_name + "!")
 #What is debugging?
     # Fixing problems in my code
 #How do you debug the different types of errors?
-    #
+    #Syntax string = 'this is my string" wrong code foretting a equal sign or comma etc 
+    # Logic Error  Where our code does something we didnt expect it to do like instead of num_1 + num_2 = 123 it would be num_1 + num_2 = 14
+    # Run-Time Error When there is a problem in the code which will break the code when it is trying to run (when it runs it breaks) variables spelled wrong
 #Describe what each of the methods below does:
-    #find()
-    ##upper()
-    #lower()
-    #strip()
+    #find() Tells you where brown is located in your variable sentence
+    ##upper() Makes everything upper case
+    #lower() Makes everything lower case
+    #strip() Removes spaces at the front and back of their symbol input
+
+
