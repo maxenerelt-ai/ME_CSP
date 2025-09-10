b7f1e6e00ace7899120a1a9be5216018a3b48245
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-10 13:22:46 -0600

    Madlib aka Silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,13 @@
+# ME 6th Silly Senteces/Madlib
+
+#Variables
+noun = input("Give me a noun/prefferably a place\n").lower()
+
+noun_2 = input("Give me another noun also a place\n").lower()
+
+verb = input("Give me a verb ending with ing\n").lower()
+
+adjective = input("Give me and adjective.\n").lower()
+
+#Print Statement
+print("If I could go anywhere in the world I would go to", noun, "and I would do some", verb, "while in a", adjective, noun_2 + ".")
