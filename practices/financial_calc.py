0de89770b2d19110cbf10e54785128d41006b7c1
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-11 01:01:53 +0000

    Ask Mrs Larose on friday

--- practices/financial_calc.py
+++ practices/financial_calc.py
@@ -1,3 +1,15 @@
 # ME 6th Financial Calculator
 
-income = input("Whats your monthly income?\n").title()
+income = float(input("whats your monthly income?\n")).title()
+
+rent = float(input("how much do you spend on rent or mortgage?\n")).title()
+
+utilities = float(input("how much do you spend on Utilities?\n")).title()
+
+groceries = float(input("how much do you spend on groceries?\n")).title()
+
+transportation = float(input("how much do you spend on transportation?\n")).title()
+
+rent_1 = (income / rent) * 100
+
+print(rent_1)
