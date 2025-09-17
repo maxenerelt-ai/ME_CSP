f1405d8b2f70182c2f9540615febfec1be7b24d2
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-17 13:43:48 -0600

    Hey

---
+++ Notes/conditionals_notes.py
@@ -0,0 +1,37 @@
+#ME 6th Conditional Notes
+
+#What puts something inside of the “if” statement?
+    #If its written under and tabbed 
+#What do if statements do?
+    # if num < 10 its a single digit number so if its false like above ten the if statement wont run
+#What are boolean statements? 
+    # The letters after if 
+num = 21
+if num < 10:
+    print(f"{num} is a single digit number")
+else:
+    print(f"{num} is not a single digit number")
+#What do else statements do?
+    # When the if statements doesnt run it runs and says your answer isnt a single digit
+#What kind of statement do you use if you have more than 2 needed outcomes?
+    # if-else-if statement (elif)
+#What do each of the different symbols mean in conditionals?
+#< less than
+#> greater than
+#<= less than or equal to
+#>= greater than or equal to
+#== it equals the same thing 3 = 3 2 because it will set a variable with one
+#! not equal
+ 
+#What are the 3 logical operators?
+    # And, True, False
+#What are logical operators for?
+    # combina conditional statements
+#What does a nested conditional statement do?
+    #allows to evaluate multiple
+
+grass = input("How tall is your grass in inches?\n").strip()
+
+if grass <= 5
+
+
--- practices/updated_helloworld.py
+++ practices/updated_helloworld.py
@@ -1,16 +1,11 @@
 # ME 6th Updated version of Hello World
 
 def name(name_1):
-    result = name
     return result
-
-name()
-name()
-name()
-name()
-name()
 
 user = input("Enter your name: ")
 result = user
 
 print("Hello there", result +"!")
+
+print(name)
