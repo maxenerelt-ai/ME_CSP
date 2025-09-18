d09e2427123abe9d59e1ca0d4ba884864b217779
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-18 13:42:27 -0600

    Hello world completed

--- practices/updated_helloworld.py
+++ practices/updated_helloworld.py
@@ -1,11 +1,12 @@
 # ME 6th Updated version of Hello World
 
 def name(name_1):
-    return result
+    print("Hello there", name_1+"!")
 
 user = input("Enter your name: ")
-result = user
 
-print("Hello there", result +"!")
-
-print(name)
+name(user)
+name("Bob")
+name("Timmy")
+name("Darius")
+name("Ja morant")
