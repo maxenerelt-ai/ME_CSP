cb876c17c6881ff948e11758b9b0f1f8fa729824
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-19 13:36:49 -0600

    finished time of day

---
+++ Notes/loops.py
@@ -0,0 +1,32 @@
+# ME 6th Loops Notes
+
+
+# What is a loop? 
+    #  
+# What are the two types of loops?
+    #  
+# What is iteration
+    #  
+# What are lists? 
+    #  
+# How do you make lists in python? 
+    #  
+# How do you make for loops in python? 
+    #  
+# How do you make while loops in python?
+    #   
+
+import datetime
+
+current = datetime.datetime.now()
+
+hour = current.hour
+
+print(f"The time is: {current}")
+
+print(f"The time is: {hour}")
+
+# lists first complex data type we learn about T^T
+
+siblings = ["Zorro", ["KK"], 21, 41, 81]
+
