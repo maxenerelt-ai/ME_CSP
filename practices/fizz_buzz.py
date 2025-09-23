425ab7b0ffeb5ceb585990e8ee67e4615b9c3014
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-09-23 13:24:30 -0600

    Fizz Buzz completed

---
+++ practices/fizz_buzz.py
@@ -0,0 +1,16 @@
+# ME 6th Fizz Buzz
+for number in range(1, 51):
+    if number % 3 == 0 and number % 5 == 0:
+        print("Fizz Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+    
+
+
+
+
+
