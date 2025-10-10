0d53650dd371606c51fe19ac25d109e651bae90c
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-10 01:06:14 +0000

    sdfasf

--- c_programming/programming/financial_calc.c
+++ c_programming/programming/financial_calc.c
@@ -4,6 +4,26 @@
 
 int main(void){
 
+    int rent[300];
+    int util[100];
+    int groceries[50];
+    int transport[50];
+    int income[100];
+
+    printf("What is your monthly income: \n");
+    scanf("%d", income);
+
+    printf("What is your monthly rent: \n");
+    scanf("%d", rent);
+    printf("What is monthly utility bill: \n");
+    scanf("%d", util);
+    printf("What is your monthly grocery bill: \n");
+    scanf("%d", groceries);
+    printf("What is your monthly transport: \n");
+    scanf("%d", transport);
+
+    int util2 = rent / income * 100;
+
 
 
 
