937596759cea7ce4380ccc827d3b006c7e591a3f
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-11 00:45:04 +0000

    asfasf

--- c_programming/silly_sentences.c
+++ c_programming/silly_sentences.c
@@ -1,2 +1,23 @@
 //ME 6th Silly Sentences / Madlibs
+#include <stdio.h>
+#include <string.h>
 
+int main(void){
+    char noun[50];
+    char verb[50];
+    char adj[50];
+
+printf("Give me a noun prefferably a place: \n");
+scanf("%s", noun);
+
+printf("Give me a verb ending with ing: \n");
+scanf("%s", verb);
+
+printf("Give me an adjective: \n");
+scanf("%s", adj);
+
+printf("The %s frog %s through %s", adj, verb, noun);
+
+
+    return 0;
+}
