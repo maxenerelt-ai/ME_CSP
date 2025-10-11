d6a79037d20e853a5331df3d7928b80f900aa9f9
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-11 00:07:07 +0000

    gSjkhasfna

--- c_programming/programming/financial_calc.c
+++ c_programming/programming/financial_calc.c
@@ -3,30 +3,32 @@
 #include <math.h>
 
 int main(void){
+    float rent;
+    float util;
+    float groceries;
+    float transport;
+    float income;
 
-    int rent[300];
-    int util[100];
-    int groceries[50];
-    int transport[50];
-    int income[100];
+    printf("What is your monthly income: ");
+    scanf("%f", &income);
+    printf("What is your monthly rent: ");
+    scanf("%f", &rent);
+    printf("What is your monthly utility bill: ");
+    scanf("%f", &util);
+    printf("What is your monthly grocery bill: ");
+    scanf("%f", &groceries);
+    printf("What is your monthly transport bill: ");
+    scanf("%f", &transport);
 
-    printf("What is your monthly income: \n");
-    scanf("%d", income);
+    float total = rent + util + groceries + transport;
+    float change = income - total;
 
-    printf("What is your monthly rent: \n");
-    scanf("%d", rent);
-    printf("What is monthly utility bill: \n");
-    scanf("%d", util);
-    printf("What is your monthly grocery bill: \n");
-    scanf("%d", groceries);
-    printf("What is your monthly transport: \n");
-    scanf("%d", transport);
+printf("Rent percentage is: %.2f\n", (rent / income) * 100);
+printf("Utility percentage is: %.2f\n", (util / income) * 100);
+printf("Grocery percentage is: %.2f\n", (rent / income) * 100);
+printf("Transport percentage is: %.2f\n", (transport / income) * 100);
+printf("Your change is: %.2f\n", change);
+printf("You should save: %.2f\n", (change / 50) * 100);
 
-    int util2 = rent / income * 100;
-
-
-
-
-    
     return 0;
 }
