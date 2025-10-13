483981216420b36c121d06d307f01e35dedec976
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-13 13:24:09 -0600

    asfasfas

---
+++ c_programming/notes/loops_notes.c
@@ -0,0 +1,33 @@
+// ME 6th Loops Notes
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(void){
+    int nums[] = {4,12443,34632,6745,5686578,67};
+
+
+    for(int x = 0; x < 11; x++){
+        printf("%d\n", nums[x]);
+    }
+
+
+    return 0;
+}
+/*
+What is a loop? 
+    Code that can be run continually until a specific condition is met / true
+What are the two types of loops?
+    For and While loops
+What is iteration
+    repetitive block of code
+What are arrays? 
+    A list of names or words
+How do you make lists in C? 
+    setting your data type, setting how many, and curly brackets, and also comma in between each example( item int num[] = {1,2,3,4,5,6}; )
+How do you make for loops in C? 
+    for(int x = 0; x < 11; x++){
+        printf("%d\n", nums[x]);
+How do you make while loops in C?
+    By using while instead of for and putting parenthesees and curly brackers
+*/
