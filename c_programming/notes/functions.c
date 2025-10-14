6cd6463ada38fc16be19ef689078ef6974b684b0
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-14 13:35:58 -0600

    kjhgfdsxcvbn

---
+++ c_programming/notes/functions.c
@@ -0,0 +1,57 @@
+// ME 6th Functions Notes
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void birthday(void){
+printf("Happy Birthday to you\n");
+printf("Happy Birthday to you\n");
+printf("Happy Birthday dear %s\n","Gage");
+printf("Happy Birthday to you\n");
+printf("Happy Birthday %s is now %d\n", "Gage", 15);
+
+}
+
+int main(void){
+birthday();
+birthday();
+
+
+    return 0;
+}
+
+
+
+
+
+/*
+What a function is
+storage containers for actions 
+Why we use functions
+Cause they can be used over and over again. Required to have a main function in lower and smaller code
+How to write a function in C
+by using either void or int and naming the function the void in parentheses and putting what you want in your curly brackets
+void birthday(void){
+printf("Happy Birthday to you\n");
+printf("Happy Birthday to you\n");
+printf("Happy Birthday dear %s\n","Gage");
+printf("Happy Birthday to you\n");
+printf("Happy Birthday %s is now %d\n", "Gage", 15);
+
+}
+
+What are arguments and parameters?
+parameters are the data type that will be put inside the arguments the argument is the thing that will go inside the parameter / replace it when running code.
+How do arguments and parameters work together?
+
+How to use parameters and arguments in python
+by naming the parameter like what data type and what is the name of the parameter call parameter and rename and it will print with the new name/argument
+What are return statements?
+ replacing the call replces the call
+How do return statements change how you define a function in C?
+you can chooose where you want to print it our
+What do return statements do with the information?
+To end a function and tell the program if it has worked
+
+
+*/
