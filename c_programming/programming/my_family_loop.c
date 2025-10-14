7ffd45d670f4c78c3e37c1b0be267db858470200
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-14 13:43:33 -0600

    adfas

---
+++ c_programming/programming/my_family_loop.c
@@ -0,0 +1,15 @@
+// ME 6th My family loop
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+char family = Max, Enyx, Soko, Zorro;
+
+for family{
+    printf("Hello");
+}
+
+
+
+    return 0;
+}
