393773321144f98c8e3516d9646deb6595326171
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-15 02:15:36 +0000

    adsfgasdg

--- c_programming/programming/fizz_buzz.c
+++ c_programming/programming/fizz_buzz.c
@@ -1,2 +1,17 @@
-// Me 6th Fizz Buzz
+// ME 6th Fizz Buzz
 #include <stdio.h>
+
+int main(void) {
+    for (int i = 1; i <= 50; i++){
+        if (i / 15 * 15 == i)
+            printf("Fizz Buzz\n");
+        else if (i / 3 * 3 == i)
+            printf("Fizz\n");
+        else if (i / 5 * 5 == i)
+            printf("Buzz\n");
+        else
+            printf("%d\n", i);
+    }
+
+    return 0;
+}
