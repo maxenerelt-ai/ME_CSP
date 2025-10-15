61056938b540acdd3f106cc259b5eefea39426d7
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-15 01:49:38 +0000

    shsdfhsjhsdfg

--- c_programming/programming/my_family_loop.c
+++ c_programming/programming/my_family_loop.c
@@ -1,2 +1,13 @@
 // ME 6th My family loop
- m  
+ #include <stdio.h>
+ #include <string.h>
+
+ int main(void){
+ const char *family[] = {"Max", "Zorro", "Enyx", "Soko"};
+
+ for (int i = 0; i < 4; i++) {
+        printf("Hello %s\n", family[i]);
+    }
+
+        return 0;
+}
