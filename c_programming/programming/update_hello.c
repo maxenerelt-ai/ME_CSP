7e435d1cdd9e573d27793a18c845d0e5d6d5ba8e
maxenerelt-ai <max.enerelt@ucas-edu.net>
2025-10-15 13:26:40 -0600

    agafd

---
+++ c_programming/programming/update_hello.c
@@ -0,0 +1,18 @@
+// ME 6th Updated Hello world
+#include <stdio.h>
+#include <string.h>
+
+void family(char name[]){
+    printf("Hello: %s\n", name);
+}
+
+int main(void){
+    family("Max");
+    family("Soko");
+    family("Enyx");
+    family("Zorro");
+
+
+
+    return 0;
+}
